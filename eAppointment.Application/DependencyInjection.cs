83c2f043295e811896f5e14d7be9ffee3dcf9dc6
huseyinncangur <huseyinncangur@gmail.com>
2024-08-25 14:04:33 +0300

    login handler

---
+++ eAppointment.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eAppointment.Application
+{
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+            return services;
+        }
+    }
+}
---
+++ eAppointment.Application/Services/IJwtProvider.cs
@@ -0,0 +1,11 @@
+﻿
+
+using eAppointment.Domain.Entities;
+
+namespace eAppointment.Application.Services
+{
+    public interface IJwtProvider
+    {
+        string CreateToken(AppUser user);
+    }
+}
